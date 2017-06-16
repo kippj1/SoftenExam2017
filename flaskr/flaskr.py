8c0e85b9e3d4dec39c09968ce9aa8bf0e670f12e
James Kippax (1000005447) <KIPPJ1@student.op.ac.nz>
2017-06-16 14:44:37 +1200

    Complete Step 6: The View Functions

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -41,6 +41,43 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries = entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('INSERT INTO entries (title, text) VALUE (?, ?)', [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 @app.teardown_appcontext
 def close_db(error):
     # Closes the databse ahain at the end of the request
