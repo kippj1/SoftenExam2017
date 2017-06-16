22d05b97e79e5f61a11c5bec145be21693bb46cb
James Kippax (1000005447) <KIPPJ1@student.op.ac.nz>
2017-06-16 14:28:31 +1200

    Complete Step 5: Creating The Database

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -23,6 +23,18 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    # Initializes the database
+    init_db()
+    print('Initialized the database.')
+
 def get_db():
     # Opens a new database connection if there is none for current application context
     if not hasattr(g, 'sqlite_db'):
